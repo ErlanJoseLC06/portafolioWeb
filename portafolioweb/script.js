f00ff7da5f958ce628b625cb09f7557e701c4b01
ErlanJoseLC06 <123124442+ErlanJoseLC06@users.noreply.github.com>
2025-03-05 11:53:49 -0500

    Delete portafolioweb directory

--- portafolioweb/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    AOS.init({
-        duration: 1000, // Duración de la animación
-        once: true, // Solo se anima una vez
-    });
-});
